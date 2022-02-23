3db46b1ae4862133e57f2a3bc6cdd66491df23b1
Andrew Makarevich <andreimakarevich2001@gmail.com>
2022-02-23 15:30:07 +0300

    feat: base functionality added

--- Stage1/src/index.js
+++ Stage1/src/index.js
@@ -1,34 +1,155 @@
+const calendar = (function () {
+  const calendarObj = {
+    events: [],
+    createEvent(name, date, callback) {
+      if (this.events.find(event => event.name === name)) {
+        throw 'Event with such name is already exists'
+      }
+      const event = {
+        name,
+        date,
+        callback
+      }
+      // const firstBreck = callback.toString().indexOf('{');
+      // const secBreck = callback.toString().lastIndexOf('}');
+      // console.log(callback.toString().slice(firstBreck + 1, secBreck))
+      Object.defineProperty(event, '_timeout', {
+        value: setTimeout(() => callback(), date.getTime() - Date.now()),
+        writable: true,
+        configurable: false,
+        enumerable: false,
+      });
+      this.events.push(event);
+      // console.log(this.events);
 
-const Calendar = {
-  events: [],
-  createEvent(name, date, callback) {
-    const event = {
-      name,
-      date,
-      callback,
-      changeExicutionTime: function (date) {
-        clearTimeout(this._timeout);
-        this._timeout = setTimeout(() => callback(), date.getTime() - Date.now());
+      // const stringifiedEvents = this.events.map(event => {
+      //   return JSON.stringify(event, function (key, value) {
+      //     if (typeof value === 'function') {
+      //       return value.toString();
+      //     }
+      //     return value;
+      //   });
+      // });
+      // localStorage.setItem('calendar-events', JSON.stringify(stringifiedEvents));
+    },
+    getEvent(name) {
+      const foundedEvent = this.events.find((event) => event.name === name);
+      if (!foundedEvent) {
+        return null;
+      }
+      function createEventCopy(eventObj) {
+        const event = JSON.parse(JSON.stringify(eventObj));
+        event.date = new Date(event.date);
+        return event;
+      }
+
+      return {
+        event: createEventCopy(foundedEvent),
+        changeExicutionTime: function (date) {
+          clearTimeout(foundedEvent._timeout);
+          foundedEvent._timeout = setTimeout(() => foundedEvent.callback(), date.getTime() - Date.now());
+          return createEventCopy(foundedEvent);
+        },
+        editEventName(name) {
+          foundedEvent.name = name;
+          return createEventCopy(foundedEvent);
+        },
+        deleteEvent: () => {
+          clearTimeout(foundedEvent._timeout);
+          this.events = this.events.filter((event) => event !== foundedEvent);
+          return;
+        }
+      }
+    },
+    getAllEvents() {
+      return JSON.parse(JSON.stringify(this.events));
+    },
+    getEventsByYear(year) {
+      const eventsByYear = JSON.parse(JSON.stringify(this.events.filter((event) => event.date.getFullYear() === year)));
+      eventsByYear.forEach(event => {
+        event.date = new Date(event.date);
+      });
+      return {
+        events: eventsByYear,
+        getEventsByMonth(month) {
+          const eventsByMonth = eventsByYear.filter((event) => event.date.getMonth() === month);
+          eventsByMonth.forEach(event => {
+            event.date = new Date(event.date);
+          });
+          return {
+            events: eventsByMonth,
+            getEventsByWeek(week) {
+              function getWeekNumber(date) {
+                const firstJan = new Date(date.getFullYear(), 0, 1);
+                const amountOfDays = Math.floor((date - firstJan) / (1000 * 60 * 60 * 24));
+                const currentWeek = Math.ceil(amountOfDays / 7);
+                return currentWeek;
+              };
+              const eventsByWeek = week ? eventsByMonth.filter((event) => getWeekNumber(event.date) === week) : eventsByMonth;
+              eventsByWeek.forEach(event => {
+                event.date = new Date(event.date);
+              });
+              return {
+                events: eventsByWeek,
+                getEventByWeekDay(day) {
+                  const eventsByWeekDay = eventsByWeek.filter((event) => event.date.getDay() === day);
+                  eventsByWeekDay.forEach(event => {
+                    event.date = new Date(event.date);
+                  });
+                  return {
+                    events: eventsByWeekDay
+                  }
+                }
+              }
+            },
+            getEventsByMonthDay(day) {
+              const eventsByMonthDay = eventsByMonth.filter((event) => event.date.getDate() === day);
+              eventsByMonthDay.forEach(event => {
+                event.date = new Date(event.date);
+              });
+              return { events: eventsByMonthDay }
+            }
+          }
+        }
       }
     }
-    Object.defineProperty(event, '_timeout', {
-      value: setTimeout(() => callback(), date.getTime() - Date.now()),
-      writable: true,
-      configurable: false,
-      enumerable: false,
-      get() {
-        return undefined
-      }
-    });
-    this.events.push(event);
-  },
-  getEvent(name) {
-    return this.events.find((event) => event.name === name);
-  },
-  getAllEvents() {
-    return this.events;
   }
-}
+  // let events = localStorage.getItem('calendar-events') ? JSON.parse(localStorage.getItem('calendar-events')) : undefined;
+  // // const te = JSON.parse("[{a:123}]");
+
+
+  // if (events && Array.isArray(events)) {
+  //   events = events.map(event => {
+  //     event = JSON.parse(event, function (key, value) {
+  //       if (key === 'callback') {
+  //         return new Function(value);
+  //       }
+  //       return value
+  //     });
+  //     event.date = new Date(event.date);
+  //     return event;
+  //   });
+  //   console.log(events);
+  //   calendarObj.events = events;
+  // }
+  const myCalendar = {
+    getEvent(name) {
+      return calendarObj.getEvent(name);
+    },
+    getAllEvents() {
+      return calendarObj.getAllEvents();
+    },
+    createEvent(name, date, callback) {
+      return calendarObj.createEvent(name, date, callback)
+    },
+    getEventsByYear(year) {
+      return calendarObj.getEventsByYear(year);
+    }
+  }
+  return myCalendar;
+})();
+
+
 
 // class Calendar {
 //   constructor() {
--- Stage1/src/test.js
+++ Stage1/src/test.js
@@ -1,10 +1,13 @@
-Calendar.createEvent('Test', new Date(Date.now() + 5000), testFunc)
-const eventObj = Calendar.getEvent('Test');
-eventObj.changeExicutionTime(new Date(Date.now() + 2000));
 
-function testFunc() {
-  console.log('Event exicute');
+function testFunc(a, b) {
+  console.log(a);
+  console.log(b)
 }
-console.log(eventObj.timeout);
-console.log(Calendar.getEvent('Test'));
-console.log(Calendar.getAllEvents());
+calendar.createEvent('Test', new Date(Date.now() + 5000), () => { testFunc('Event', 'exicuted') });
+calendar.getEvent('Test').changeExicutionTime(new Date(Date.now() + 10000));
+console.log(calendar.getAllEvents());
+// calendar.getEvent('Test').deleteEvent();
+console.log(calendar.getEvent('Test').editEventName('Test1'));
+// let events = calendar.getAllEvents();
+// events = 'NO ARRAY!';
+console.log(calendar.getEventsByYear(2022).getEventsByMonth(1).getEventsByMonthDay(23));
