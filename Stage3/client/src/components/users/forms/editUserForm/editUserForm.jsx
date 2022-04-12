f912dea626f6e7a30265cfae448f248919d6db68
Andrew Makarevich <andreimakarevich2001@gmail.com>
2022-04-12 16:08:44 +0300

    feat: edit user form controlled inputs added, clear changes btn added

--- Stage3/client/src/components/users/forms/editUserForm/editUserForm.jsx
+++ Stage3/client/src/components/users/forms/editUserForm/editUserForm.jsx
@@ -5,10 +5,10 @@ import useDelayState from '../../../../hooks/useDelayState';
 
 const EditUserForm = ({ userObj }) => {
   const [newUserInfo, setNewUserInfo] = useState({
-    username: undefined,
-    firstName: undefined,
-    lastName: undefined,
-    email: undefined,
+    username: '',
+    firstName: '',
+    lastName: '',
+    email: '',
   });
 
   useEffect(() => {
@@ -35,16 +35,38 @@ const EditUserForm = ({ userObj }) => {
         <MyInputWithLabel
           labelText={"User's first name"}
           value={newUserInfo.firstName}
+          onChange={(e) => {
+            setNewUserInfo({ ...newUserInfo, firstName: e.target.value });
+          }}
         />
         <MyInputWithLabel
           labelText={"User's last name"}
           value={newUserInfo.lastName}
+          onChange={(e) => {
+            setNewUserInfo({ ...newUserInfo, lastName: e.target.value });
+          }}
         />
         <MyInputWithLabel
           labelText={"User's email"}
           value={newUserInfo.email}
+          onChange={(e) => {
+            setNewUserInfo({ ...newUserInfo, email: e.target.value });
+          }}
         />
       </div>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          setNewUserInfo({
+            username: userObj.username,
+            firstName: userObj.firstName,
+            lastName: userObj.lastName,
+            email: userObj.email,
+          });
+        }}
+      >
+        Clear changes
+      </button>
       <button>Submit changes</button>
     </form>
   );
